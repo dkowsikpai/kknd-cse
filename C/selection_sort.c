a951631a882e28f6326be365937a886804395793
Kowsik Nandagopan D <43445626+dkowsikpai@users.noreply.github.com>
2019-05-07 21:05:50 +0530

    Create selection_sort.c

---
+++ C/selection_sort.c
@@ -0,0 +1,44 @@
+//Selection sort program for sorting in acending order.
+#include <stdio.h>
+
+void man]in(){//Selection sort program for sorting in acending order.
+#include <stdio.h>
+
+void main(){
+  int a[20], i, j, n, min_ind, temp;
+  printf("Enter the total no in array:");
+  scanf("%d", &n);
+  printf("Enter the array:\n");
+  for (i=0; i<n; i++){
+	  scanf("%d" , &a[i]);
+  }
+  for (i=0; i<n; i++){
+    min_ind = i;
+    for (j=i+1; j<n; j++){
+      if (a[j] < a[min_ind])min_ind = j;
+    }
+    temp = a[min_ind];
+    a[min_ind] = a[i];
+    a[i] = temp;
+  }
+  for (i=0; i<n; i++){
+    printf("%d",  a[i]);
+  }
+}
+  int a[20], i, j, n, min_ind, temp;
+  printf("Enter the total no in array:");
+  scanf("%d", &n);
+  printf("Enter the array:\n");
+  for (i=0; i<n; i++){
+    min_ind = i;
+    for (j=i+1; j<n; j++){
+      if (a[j] < a[min_ind])min_ind = j;
+    }
+    temp = a[min_ind];
+    a[min_ind] = a[i];
+    a[i] = temp;
+  }
+  for (i=0; i<n; i++){
+    printf("%d",  a[i]);
+  }
+}
